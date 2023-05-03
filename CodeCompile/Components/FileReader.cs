cf51cc69f9696fa3cddd635f5dfb80f6270ff4f6
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-03 20:45:40 +0700

    Add FileReader.cs  - Add methods for reading files.

---
+++ CodeCompile/Components/FileReader.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeCompile.Components
+{
+    /// <summary>
+    /// Provides methods for reading files.
+    /// </summary>
+    static class FileReader
+    {
+        /// <summary>
+        /// Read all text in the given file.
+        /// </summary>
+        /// <param name="filename">File's name to read.</param>
+        /// <returns>Full file's content.</returns>
+        public static string ReadFullFile(string filename)
+        {
+            string fileContent = string.Empty;
+
+            try
+            {
+                fileContent = File.ReadAllText(filename);
+            }
+            catch (FileNotFoundException) { }
+            catch (DirectoryNotFoundException) { }
+
+            return fileContent;
+        }
+
+        /// <summary>
+        /// Read all text in the file in the same folder with application.
+        /// </summary>
+        /// <param name="filename">File's name to read.</param>
+        /// <returns>Full file's content.</returns>
+        public static string ReadFullFileSameApplicationDir(string filename)
+        {
+            string applicationPath = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(applicationPath, filename);
+
+            return ReadFullFile(filePath);
+        }
+    }
+}
